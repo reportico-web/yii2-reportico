63f3f3d34cefc6283383f417cfdd2008189c95f7
peterd <peterdeed@nimbusjourneyinfo.co.uk>
2014-08-26 21:52:00 +0100

    Initial LOad

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace reportico\reportico;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
