35c9fab89e2657b1bc723b2159b583f66ead7974
Peter Deed <info@reportico.org>
2015-04-12 10:44:03 +0100

    4.3 fix

--- components/tcpdf/include/tcpdf_fonts.php
+++ components/tcpdf/include/tcpdf_fonts.php
@@ -1604,9 +1604,9 @@ class TCPDF_FONTS {
 	 */
 	public static function UTF8ArrayToUniArray($ta, $isunicode=true) {
 		if ($isunicode) {
-			return array_map(array('Reportico\Reportico\TCPDF_FONTS', 'unichrUnicode'), $ta);
+			return array_map(array('reportico\reportico\components\TCPDF_FONTS', 'unichrUnicode'), $ta);
 		}
-		return array_map(array('Reportico\Reportico\TCPDF_FONTS', 'unichrASCII'), $ta);
+		return array_map(array('reportico\reportico\components\TCPDF_FONTS', 'unichrASCII'), $ta);
 	}
 
 	/**
@@ -1883,7 +1883,7 @@ class TCPDF_FONTS {
 		if ($isunicode) {
 			// requires PCRE unicode support turned on
 			$chars = TCPDF_STATIC::pregSplit('//','u', $str, -1, PREG_SPLIT_NO_EMPTY);
-			$carr = array_map(array('Reportico\Reportico\TCPDF_FONTS', 'uniord'), $chars);
+			$carr = array_map(array('reportico\reportico\components\TCPDF_FONTS', 'uniord'), $chars);
 		} else {
 			$chars = str_split($str);
 			$carr = array_map('ord', $chars);
